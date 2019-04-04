16df9d0ed264dc47f90bd7c788b195ded749738e
HiKeng <HiKeng@users.noreply.github.com>
2019-04-05 01:27:42 +0700

    Project Backup

---
+++ 2D Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform Player;
+    public float ZOffset;
+    public float YOffset; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if(Player)
+        {
+            ZOffset = transform.position.z - Player.position.z;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 TargetPosition = Player.transform.position + Vector3.forward * ZOffset;
+        TargetPosition.y += YOffset;
+
+        Vector3 Velocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, TargetPosition, ref Velocity, 0.1f);
+
+
+    }
+}
---
+++ 2D Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int HP = 100;
+    public float WalkSpeed = 300.5f;
+    public float JumpForce = 500f;
+    public SpriteRenderer PlayerSprite;
+
+    public Animator anim;
+
+    public Transform GroundChecker;
+
+    public bool isGrounded;
+    public float groundCheckRange = 1f;
+    public LayerMask groundLayer;
+
+    public bool isWalking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * -WalkSpeed * Time.deltaTime);
+            PlayerSprite.flipX = true;
+            if(isGrounded == true)
+            {
+                anim.SetBool("isWalk", true);
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * WalkSpeed * Time.deltaTime);
+            PlayerSprite.flipX = false;
+
+            if (isGrounded == true)
+            {
+                anim.SetBool("isWalk", true);
+            }
+        }
+        
+        if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("isWalk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce); 
+        }
+
+        //isGrounded = Physics.Raycast(transform.position, -Vector3.up, groundCheckRange, groundLayer);
+        //Debug.DrawRay(transform.position, Vector3.up * groundCheckRange);
+
+        isGrounded = Physics.CheckSphere(GroundChecker.position, groundCheckRange, groundLayer);
+
+        anim.SetBool("Grounded", isGrounded);
+
+        if(isGrounded)
+        {
+          //  GetComponentInChildren<SpriteRenderer>().color = Color.white;
+
+        } else
+        {
+            //GetComponentInChildren<SpriteRenderer>().color = Color.red;
+        }
+    }
+}
